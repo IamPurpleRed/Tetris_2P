3a71cc6b6831cacbfbbb75cf024ad147cb798ee7
PurpleRed <benson060211@gmail.com>
2021-06-20 23:46:04 +0800

    1.0.1 修正

--- FinalProject/AboutPage.Designer.cs
+++ FinalProject/AboutPage.Designer.cs
@@ -47,9 +47,10 @@ namespace FinalProject
             // 
             this.title_deco.BackColor = System.Drawing.Color.Fuchsia;
             this.title_deco.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title_deco.Location = new System.Drawing.Point(15, 20);
+            this.title_deco.Location = new System.Drawing.Point(28, 35);
+            this.title_deco.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.title_deco.Name = "title_deco";
-            this.title_deco.Size = new System.Drawing.Size(10, 33);
+            this.title_deco.Size = new System.Drawing.Size(18, 58);
             this.title_deco.TabIndex = 3;
             this.title_deco.Text = " ";
             // 
@@ -57,9 +58,10 @@ namespace FinalProject
             // 
             this.title.AutoSize = true;
             this.title.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(31, 20);
+            this.title.Location = new System.Drawing.Point(57, 35);
+            this.title.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(100, 33);
+            this.title.Size = new System.Drawing.Size(175, 59);
             this.title.TabIndex = 2;
             this.title.Text = "ABOUT";
             // 
@@ -67,9 +69,10 @@ namespace FinalProject
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(17, 71);
+            this.label1.Location = new System.Drawing.Point(31, 124);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 26);
+            this.label1.Size = new System.Drawing.Size(170, 47);
             this.label1.TabIndex = 4;
             this.label1.Text = "Tetris 2P";
             // 
@@ -77,9 +80,10 @@ namespace FinalProject
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(17, 255);
+            this.label2.Location = new System.Drawing.Point(31, 446);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 26);
+            this.label2.Size = new System.Drawing.Size(257, 47);
             this.label2.TabIndex = 5;
             this.label2.Text = "Special Thanks";
             // 
@@ -87,19 +91,21 @@ namespace FinalProject
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(26, 109);
+            this.label3.Location = new System.Drawing.Point(48, 191);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 23);
+            this.label3.Size = new System.Drawing.Size(188, 39);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Version: 1.0";
+            this.label3.Text = "Version: 1.0.1";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(26, 132);
+            this.label4.Location = new System.Drawing.Point(48, 231);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(257, 23);
+            this.label4.Size = new System.Drawing.Size(447, 39);
             this.label4.TabIndex = 7;
             this.label4.Text = "Made by PurpleRed in June, 2021";
             // 
@@ -107,9 +113,10 @@ namespace FinalProject
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(26, 155);
+            this.label5.Location = new System.Drawing.Point(48, 271);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(501, 23);
+            this.label5.Size = new System.Drawing.Size(890, 39);
             this.label5.TabIndex = 8;
             this.label5.Text = "Pay tribute to Tetris Battle, one of my favorite games in Facebook.";
             // 
@@ -117,9 +124,10 @@ namespace FinalProject
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(26, 178);
+            this.label6.Location = new System.Drawing.Point(48, 312);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(706, 23);
+            this.label6.Size = new System.Drawing.Size(1258, 39);
             this.label6.TabIndex = 9;
             this.label6.Text = "If you want to know more like the rules, control keys, release log...etc. Please " +
     "go to my GitHub: ";
@@ -128,9 +136,10 @@ namespace FinalProject
             // 
             this.github_link.AutoSize = true;
             this.github_link.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.github_link.Location = new System.Drawing.Point(26, 201);
+            this.github_link.Location = new System.Drawing.Point(48, 352);
+            this.github_link.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.github_link.Name = "github_link";
-            this.github_link.Size = new System.Drawing.Size(354, 23);
+            this.github_link.Size = new System.Drawing.Size(624, 39);
             this.github_link.TabIndex = 10;
             this.github_link.TabStop = true;
             this.github_link.Text = "https://github.com/PurpleRed0602/Tetris_2P";
@@ -140,9 +149,10 @@ namespace FinalProject
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(26, 294);
+            this.label7.Location = new System.Drawing.Point(48, 514);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(662, 23);
+            this.label7.Size = new System.Drawing.Size(1189, 39);
             this.label7.TabIndex = 11;
             this.label7.Text = "Thanks for those who test my game when it\'s unstable. I can\'t finish it without t" +
     "heir help.";
@@ -151,19 +161,20 @@ namespace FinalProject
             // 
             this.special_thanks_list.Enabled = false;
             this.special_thanks_list.Font = new System.Drawing.Font("華康手札體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.special_thanks_list.Location = new System.Drawing.Point(30, 320);
+            this.special_thanks_list.Location = new System.Drawing.Point(55, 560);
+            this.special_thanks_list.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.special_thanks_list.Multiline = true;
             this.special_thanks_list.Name = "special_thanks_list";
             this.special_thanks_list.ReadOnly = true;
-            this.special_thanks_list.Size = new System.Drawing.Size(658, 122);
+            this.special_thanks_list.Size = new System.Drawing.Size(1203, 210);
             this.special_thanks_list.TabIndex = 12;
             this.special_thanks_list.TabStop = false;
             // 
             // AboutPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(742, 469);
+            this.ClientSize = new System.Drawing.Size(1360, 821);
             this.Controls.Add(this.special_thanks_list);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.github_link);
@@ -176,6 +187,7 @@ namespace FinalProject
             this.Controls.Add(this.title_deco);
             this.Controls.Add(this.title);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutPage";
